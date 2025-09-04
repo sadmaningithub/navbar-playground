d886af265741298e0c7a749f48590def22314aad
sadmaningithub <sadmanahmed261@gmail.com>
2025-09-04 14:18:46 +0600

    add styles to the navigation links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,15 @@ import { NavLink } from "react-router";
 const NavBar = () => {
 
     const navLinks = [
-        { name: 'home', icon: <House className=" transform hover:scale-125 transition ease-in-out duration-300"/> },
-        { name: 'about', icon: <BookOpen className=" transform hover:scale-125 transition ease-in-out duration-300"/>},
-        { name: 'contact', icon: <Mail className=" transform hover:scale-125 transition ease-in-out duration-300 " />}
+        { name: 'home', icon: <House className=" " /> },
+        { name: 'about', icon: <BookOpen className=" " /> },
+        { name: 'contact', icon: <Mail className="  " /> }
     ]
 
     return (
         <div className="flex flex-col space-y-2 items-center max-w-10 w-full ">
             {
-                navLinks.map(navLink => <NavLink to={navLink.name}> {navLink.icon}</NavLink>)
+                navLinks.map(navLink => <NavLink className={({ isActive }) => isActive ? "transform scale-130" : "transform hover:scale-125 hover:opacity-70 transition ease-in-out duration-300"} to={navLink.name}> {navLink.icon}</NavLink>)
             }
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 
 
+
 const Home = () => {
     return (
         <div>
             <h1>This is homepage.</h1>
+            
         </div>
     );
 };
